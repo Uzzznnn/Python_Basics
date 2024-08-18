d6a6c87bd7fd1a588e2ac756c2641b430dfec7a9
Uzzznnn <020bim063@sxc.edu.np>
2024-08-18 19:18:45 +0545

    New branch and cal

--- Adv_python/Object_Oriented_program.py
+++ Adv_python/Object_Oriented_program.py
@@ -1,10 +1,42 @@
-class Animal: #class always on top
+# class Animal: #class always on top
   
-  def what_sound_does_it_make(self,flag):
-    if flag.lower()=='bark':
-      print("it's a dog")
-    else:
-      print("it's a human")
+#   def what_sound_does_it_make(self,flag):
+#     if flag.lower()=='bark':
+#       print("it's a dog")
+#     else:
+#       print("it's a human")
       
-species_1= Animal()
-species_1.what_sound_does_it_make('Bark')
+# species_1= Animal()
+# species_1.what_sound_does_it_make('Bark')
+
+#To make a calculator that must add subtract multiply divide two numbers etc
+
+class Calculator:
+
+    def add(self,x,y):
+
+        print(f"The sum is {x+y}")
+
+    def subtract(self,x,y):
+
+        print(f"The difference is {x-y}")
+
+    def multiply(self,x,y):
+
+        print(f"The product is {x*y}")
+
+    def divide(self,x,y):
+
+        print(f"The division is {x/y}")
+
+instance1 = Calculator()
+
+instance2 = Calculator()
+
+instance1.add(4,9)
+
+instance1.subtract(18,11)
+
+instance2.multiply(6,8)
+
+instance2.divide(98,5)
